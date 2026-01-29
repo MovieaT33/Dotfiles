73abd3e8b7b3aab8e02a24174f092609556d0c9d
MovieaT33 <github.drinking886@passinbox.com>
2026-01-29 13:31:35 +0200

    fix: update machine configuration

--- modules/machine.nix
+++ modules/machine.nix
@@ -1,17 +1,11 @@
 { ... }:
 
-{
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    nixosConfigurations."nixos" = pkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./hardware.nix
-        ./bootloader.nix
-        ./system.nix
-      ];
-    };
+let
+  system = "x86_64-linux";
+  pkgs = import nixpkgs { inherit system; };
+in {
+  nixosConfigurations."nixos" = pkgs.lib.nixosSystem {
+    inherit system;
+    modules = [ ./hardware.nix ./bootloader.nix ./system.nix ];
   };
 }
