c5103b58a54e7b5b433888c076f0b0c9821c0d27
MovieaT33 <github.drinking886@passinbox.com>
2026-01-29 14:09:36 +0200

    fix: flake

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
       import-tree.url = "github:vic/import-tree";
   };
 
-  outputs = { nixpkgs, ... } @ inputs: let
+  outputs = { nixpkgs, import-tree, ... } @ inputs: let
     system = "x86_64-linux";
   in {
     nixosConfigurations = {
       laptop = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = import-tree ./hosts/laptop ++ import-tree ./modules;
+        modules = builtins.attrValues (import-tree ./hosts/laptop)
+               ++ builtins.attrValues (import-tree ./modules);
       };
     };
   };
